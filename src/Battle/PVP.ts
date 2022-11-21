c1e1f325d1b945f2aee05ecfb3a58a569caf12ee
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2022-11-21 20:15:08 -0300

    requisito 10 feito

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(private p1: Fighter, private p2: Fighter) {
+    super(p1);
+  }
+
+  fight(): number {
+    for (let i = 0; i === 0;) {
+      this.p1.attack(this.p2);
+      this.p2.attack(this.p1);
+    
+      if (this.p1.lifePoints === -1 || this.p2.lifePoints === -1) {
+        i = 1;
+        const result = super.fight();
+        return result;
+      }
+    }
+  }
+}
