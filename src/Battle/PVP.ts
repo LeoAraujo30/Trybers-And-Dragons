17fccbbf04f89225ecc406f105d555ce30e7a548
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2022-11-21 20:18:17 -0300

    corrigindo requisito 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -7,15 +7,16 @@ export default class PVP extends Battle {
   }
 
   fight(): number {
+    let result = 0;
     for (let i = 0; i === 0;) {
       this.p1.attack(this.p2);
       this.p2.attack(this.p1);
     
       if (this.p1.lifePoints === -1 || this.p2.lifePoints === -1) {
         i = 1;
-        const result = super.fight();
-        return result;
+        result = super.fight();
       }
     }
+    return result;
   }
 }
